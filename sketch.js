cedf99828b237ad0ae048a83959891f8af39ddbd
rrrrodrrrrigo <yectocsalev"gmail.com>
2019-10-25 12:57:00 -0400

    tzontli

--- sketch.js
+++ sketch.js
@@ -18,13 +18,14 @@ function preload() {
     tzontli[5] = loadImage("img/tzontli_5.svg");
     tzontli[6] = loadImage("img/tzontli_6.svg");
     tzontli[7] = loadImage("img/tzontli_7.svg");
+    tzontli[8] = loadImage("img/tzontli_8.svg");
 
 }
 
 let config = {
 
-    noche: false,
-    dia: true,
+    noche: true,
+    dia: false,
     amanece: false,
 }
 
@@ -74,10 +75,10 @@ function draw() {
     imageMode(CENTER);
 
     var ruidoC = 0.0004;
-    var posC = 100 + noise(millis() * ruidoC) * 70;
+    var posC = 70 + noise(millis() * ruidoC) * 90;
 
-    var ruidoC1 = 0.00001;
-    var posC1 = 100 + noise(millis() * ruidoC) * 100;
+    var ruidoC1 = 0.00008;
+    var posC1 = 60 + noise(millis() * ruidoC1) * 90;
 
 
     let jump = 120;
@@ -86,73 +87,73 @@ function draw() {
 
     //scale(posC);
 
-    image(tzontli[1], 100, 700, posC, posC);
-    image(tzontli[2], 100 + jump, 700, posC1, posC1);
-    image(tzontli[3], 100 + jump * 2, 700, posC, posC);
-    image(tzontli[4], 100 + jump * 3, 700, posC1, posC1);
-    image(tzontli[5], 100 + jump * 4, 700, posC, posC);
-    image(tzontli[6], 100 + jump * 5, 700, posC1, posC1);
-    image(tzontli[7], 100 + jump * 6, 700, posC, posC);
-    image(tzontli[1], 100 + jump * 7, 700, posC1, posC1);
-    image(tzontli[2], 100 + jump * 8, 700, posC, posC);
-    image(tzontli[3], 100 + jump * 9, 700, posC1, posC1);
+    image(tzontli[5], 100, 700, posC, posC);
+    image(tzontli[6], 100 + jump, 700, posC1, posC1);
+    image(tzontli[4], 100 + jump * 2, 700, posC1, posC1);
+    image(tzontli[1], 100 + jump * 3, 700, posC, posC);
+    image(tzontli[7], 100 + jump * 4, 700, posC, posC);
+    image(tzontli[3], 100 + jump * 5, 700, posC1, posC1);
+    image(tzontli[2], 100 + jump * 6, 700, posC1, posC1);
+    image(tzontli[8], 100 + jump * 7, 700, posC, posC);
+    image(tzontli[5], 100 + jump * 8, 700, posC1, posC1);
+    image(tzontli[6], 100 + jump * 9, 700, posC, posC);
 
-    image(tzontli[7], 100, 600, posC, posC);
-    image(tzontli[1], 100 + jump, 600, posC1, posC1);
-    image(tzontli[2], 100 + jump * 2, 600, posC, posC);
-    image(tzontli[3], 100 + jump * 3, 600, posC1, posC1);
-    image(tzontli[4], 100 + jump * 4, 600, posC, posC);
-    image(tzontli[5], 100 + jump * 5, 600, posC1, posC1);
+    image(tzontli[1], 100, 600, posC, posC);
+    image(tzontli[4], 100 + jump, 600, posC1, posC1);
+    image(tzontli[5], 100 + jump * 2, 600, posC, posC);
+    image(tzontli[8], 100 + jump * 3, 600, posC1, posC1);
+    image(tzontli[3], 100 + jump * 4, 600, posC1, posC1);
+    image(tzontli[2], 100 + jump * 5, 600, posC, posC);
     image(tzontli[6], 100 + jump * 6, 600, posC, posC);
-    image(tzontli[7], 100 + jump * 7, 600, posC1, posC1);
-    image(tzontli[1], 100 + jump * 8, 600, posC, posC);
-    image(tzontli[2], 100 + jump * 9, 600, posC1, posC1);
+    image(tzontli[3], 100 + jump * 7, 600, posC1, posC1);
+    image(tzontli[7], 100 + jump * 8, 600, posC, posC);
+    image(tzontli[5], 100 + jump * 9, 600, posC1, posC1);
 
-    image(tzontli[6], 100, 500, posC, posC);
-    image(tzontli[7], 100 + jump, 500, posC1, posC1);
-    image(tzontli[1], 100 + jump * 2, 500, posC, posC);
-    image(tzontli[2], 100 + jump * 3, 500, posC1, posC1);
-    image(tzontli[3], 100 + jump * 4, 500, posC, posC);
-    image(tzontli[4], 100 + jump * 5, 500, posC1, posC1);
-    image(tzontli[5], 100 + jump * 6, 500, posC, posC);
-    image(tzontli[6], 100 + jump * 7, 500, posC1, posC1);
-    image(tzontli[7], 100 + jump * 8, 500, posC, posC);
-    image(tzontli[1], 100 + jump * 9, 500, posC1, posC1);
+    image(tzontli[8], 100, 500, posC1, posC1);
+    image(tzontli[3], 100 + jump, 500, posC, posC);
+    image(tzontli[4], 100 + jump * 2, 500, posC1, posC1);
+    image(tzontli[5], 100 + jump * 3, 500, posC1, posC1);
+    image(tzontli[7], 100 + jump * 4, 500, posC, posC);
+    image(tzontli[6], 100 + jump * 5, 500, posC, posC);
+    image(tzontli[1], 100 + jump * 6, 500, posC1, posC1);
+    image(tzontli[2], 100 + jump * 7, 500, posC1, posC1);
+    image(tzontli[3], 100 + jump * 8, 500, posC, posC);
+    image(tzontli[4], 100 + jump * 9, 500, posC1, posC1);
 
-    image(tzontli[5], 100, 400, posC, posC);
+    image(tzontli[7], 100, 400, posC, posC);
     image(tzontli[6], 100 + jump, 400, posC1, posC1);
-    image(tzontli[7], 100 + jump * 2, 400, posC, posC);
-    image(tzontli[1], 100 + jump * 3, 400, posC1, posC1);
-    image(tzontli[2], 100 + jump * 4, 400, posC, posC);
-    image(tzontli[3], 100 + jump * 5, 400, posC1, posC1);
+    image(tzontli[5], 100 + jump * 2, 400, posC1, posC1);
+    image(tzontli[2], 100 + jump * 3, 400, posC, posC);
+    image(tzontli[1], 100 + jump * 4, 400, posC, posC);
+    image(tzontli[8], 100 + jump * 5, 400, posC1, posC1);
     image(tzontli[4], 100 + jump * 6, 400, posC, posC);
-    image(tzontli[5], 100 + jump * 7, 400, posC1, posC1);
-    image(tzontli[6], 100 + jump * 8, 400, posC, posC);
-    image(tzontli[7], 100 + jump * 9, 400, posC1, posC1);
+    image(tzontli[6], 100 + jump * 7, 400, posC1, posC1);
+    image(tzontli[7], 100 + jump * 8, 400, posC1, posC1);
+    image(tzontli[3], 100 + jump * 9, 400, posC, posC);
 
-    image(tzontli[4], 100, 300, posC, posC);
-    image(tzontli[5], 100 + jump, 300, posC1, posC1);
-    image(tzontli[6], 100 + jump * 2, 300, posC, posC);
-    image(tzontli[7], 100 + jump * 3, 300, posC1, posC1);
-    image(tzontli[1], 100 + jump * 4, 300, posC, posC);
-    image(tzontli[2], 100 + jump * 5, 300, posC1, posC1);
-    image(tzontli[3], 100 + jump * 6, 300, posC, posC);
-    image(tzontli[4], 100 + jump * 7, 300, posC1, posC1);
+    image(tzontli[5], 100, 300, posC, posC);
+    image(tzontli[6], 100 + jump, 300, posC1, posC1);
+    image(tzontli[4], 100 + jump * 2, 300, posC, posC);
+    image(tzontli[1], 100 + jump * 3, 300, posC1, posC1);
+    image(tzontli[7], 100 + jump * 4, 300, posC, posC);
+    image(tzontli[3], 100 + jump * 5, 300, posC1, posC1);
+    image(tzontli[2], 100 + jump * 6, 300, posC, posC);
+    image(tzontli[8], 100 + jump * 7, 300, posC1, posC1);
     image(tzontli[5], 100 + jump * 8, 300, posC, posC);
     image(tzontli[6], 100 + jump * 9, 300, posC1, posC1);
 
-    image(tzontli[3], 100, 200, posC, posC);
+    image(tzontli[1], 100, 200, posC, posC);
     image(tzontli[4], 100 + jump, 200, posC1, posC1);
     image(tzontli[5], 100 + jump * 2, 200, posC, posC);
-    image(tzontli[6], 100 + jump * 3, 200, posC1, posC1);
-    image(tzontli[7], 100 + jump * 4, 200, posC, posC);
-    image(tzontli[1], 100 + jump * 5, 200, posC1, posC1);
-    image(tzontli[2], 100 + jump * 6, 200, posC, posC);
+    image(tzontli[8], 100 + jump * 3, 200, posC1, posC1);
+    image(tzontli[3], 100 + jump * 4, 200, posC, posC);
+    image(tzontli[2], 100 + jump * 5, 200, posC1, posC1);
+    image(tzontli[6], 100 + jump * 6, 200, posC, posC);
     image(tzontli[3], 100 + jump * 7, 200, posC1, posC1);
-    image(tzontli[4], 100 + jump * 8, 200, posC, posC);
+    image(tzontli[7], 100 + jump * 8, 200, posC, posC);
     image(tzontli[5], 100 + jump * 9, 200, posC1, posC1);
 
-    image(tzontli[2], 100, 100, posC, posC);
+    image(tzontli[8], 100, 100, posC, posC);
     image(tzontli[3], 100 + jump, 100, posC1, posC1);
     image(tzontli[4], 100 + jump * 2, 100, posC, posC);
     image(tzontli[5], 100 + jump * 3, 100, posC1, posC1);
@@ -205,10 +206,10 @@ function noiseField_noche() {
 
     for (var y = 0; y < height; y += density) {
         for (var x = 0; x < width; x += density) {
-            var n = noise(xnoise, ynoise, znoise) * 255;
+            var n = noise(xnoise, ynoise, znoise) * 120;
             noStroke();
             //fill(contador, n, contador);
-            fill(50, n, 200);
+            fill(n, n, n);
             //fill(contador, 158, n);
             //fill(255, 178, n);
 
@@ -253,11 +254,11 @@ function keyPressed() {
 
     //
 
-    if ((key == '1') || (key == '1')) {
+    if ((key == '2') || (key == '2')) {
         config.dia = !config.dia;
     }
 
-    if ((key == '2') || (key == '2')) {
+    if ((key == '1') || (key == '1')) {
         config.noche = !config.noche;
     }
 
